0dd5fc863fd33e5fc52520ccc17d98ead5825b01
Tyler Marcott <tylerjmarcott@gmail.com>
2023-10-20 10:42:59 -0600

    front and backend are spun up

---
+++ KeeprFinalCheckpoint.client/src/env.js
@@ -0,0 +1,8 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'dev-r2l4kl7ifqatct23.us.auth0.com'
+export const clientId = '3ugFmbTisZI8MvM7ux0qRbSLZS8X8Zo1'
+export const audience = 'https://tylerApi.com'
+
+// NOTE: MAKE SURE TO START YOUR DB!!!!!!!
