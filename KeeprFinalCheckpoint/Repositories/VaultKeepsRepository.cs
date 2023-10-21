5775248578b9357a134f06b220a5872a1e28e16e
Tyler Marcott <tylerjmarcott@gmail.com>
2023-10-21 13:38:44 -0600

    working on creating vaultkeep

--- KeeprFinalCheckpoint/Repositories/VaultKeepsRepository.cs
+++ KeeprFinalCheckpoint/Repositories/VaultKeepsRepository.cs
@@ -16,8 +16,24 @@ public class VaultKeepsRepository : IRepository<VaultKeep, int>
     {
         string sql = @"
         INSERT INTO vaultKeeps
+        (vaultId, keepId)
+        VALUES
+        (@vaultId, @keepId);
+
+        SELECT
+            vaultKeeps.*,
+            accounts.*
+        FROM vaultKeeps
+        JOIN accounts ON accounts.id = vaultKeeps.creatorId
+        WHERE vaultKeeps.id = LAST_INSERT_ID()
         ;";
+        int lastInsertId = _db.ExecuteScalar<int>(sql, newData);
+        newData.Id = lastInsertId;
+        return newData;
     }
+
+    // NOTE: ok, so this is how the data relationship works with VaultKeep: Vaultkeep points at keep, vault, and account. That means we need to Join with creator, keep, and vault I believe? I feel like Mick said that we only needed to do 2 joins, but maybe we have to actually do 3. That would make the most sense to me. I believe a good example of this is cult membership/ cultist in the Instacult example.
+    //NOTE; vaultKeeps are just keeps that are put into a vault. It's almost like a copy, where we are taking who is making the vaultKeep (user/account), which keep they want to save, in which vault. That is the many-to-many relationship, between vaultkeep and the 3 others.
 
     public int Delete(int id)
     {
