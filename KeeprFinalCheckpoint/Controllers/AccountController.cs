6810c5309baae1026d63f83b09d31db2bfdb16fa
Tyler Marcott <tylerjmarcott@gmail.com>
2023-10-22 15:55:49 -0600

    need to do vaultKeeps first.

--- KeeprFinalCheckpoint/Controllers/AccountController.cs
+++ KeeprFinalCheckpoint/Controllers/AccountController.cs
@@ -27,4 +27,21 @@ public class AccountController : ControllerBase
       return BadRequest(e.Message);
     }
   }
+
+  // TODO: I need to wait to do this until the vaultkeeps are done, because I need to use vaultkeeps in order to get this data I think. Note, ref for this is in AccountController.cs in PostItSharp
+
+  // [Authorize]
+  // [HttpGet("vaults")]
+  // public async Task<ActionResult<Account>> GetVaultsByAccount()
+  // {
+  //   try
+  //   {
+  //     Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+  //     return Ok(_accountService.GetOrCreateProfile(userInfo));
+  //   }
+  //   catch (Exception e)
+  //   {
+  //     return BadRequest(e.Message);
+  //   }
+  // }
 }
